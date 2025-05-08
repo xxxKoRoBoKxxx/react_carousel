239f0b99f0c2d7f5fbc3494b8ea6d82ea53e57da
Andrii Filipenko <andreyfil1337@gmail.com>
2025-05-08 17:21:00 +0300

    in progress

--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,87 @@ import React from 'react';
 import './App.scss';
 import Carousel from './components/Carousel';
 
+type Image = {
+  src: string;
+  alt: number;
+};
+
 interface State {
-  images: string[];
+  images: Image[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+  infinite: boolean;
 }
 
 class App extends React.Component<{}, State> {
   state = {
     images: [
-      './img/1.png',
-      './img/2.png',
-      './img/3.png',
-      './img/4.png',
-      './img/5.png',
-      './img/6.png',
-      './img/7.png',
-      './img/8.png',
-      './img/9.png',
-      './img/10.png',
+      {
+        src: './img/1.png',
+        alt: 1,
+      },
+      {
+        src: './img/2.png',
+        alt: 2,
+      },
+      {
+        src: './img/3.png',
+        alt: 3,
+      },
+      {
+        src: './img/4.png',
+        alt: 4,
+      },
+      {
+        src: './img/5.png',
+        alt: 5,
+      },
+      {
+        src: './img/6.png',
+        alt: 6,
+      },
+      {
+        src: './img/7.png',
+        alt: 7,
+      },
+      {
+        src: './img/8.png',
+        alt: 8,
+      },
+      {
+        src: './img/9.png',
+        alt: 9,
+      },
+      {
+        src: './img/10.png',
+        alt: 10,
+      },
     ],
+    step: 3,
+    frameSize: 3,
+    itemWidth: 130,
+    animationDuration: 1000,
+    infinite: false,
   };
 
   render() {
-    const { images } = this.state;
+    const { images, step, frameSize, itemWidth, animationDuration, infinite } =
+      this.state;
 
     return (
       <div className="App">
-        {/* eslint-disable-next-line */}
         <h1>Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <Carousel
+          images={images}
+          step={step}
+          frameSize={frameSize}
+          itemWidth={itemWidth}
+          animationDuration={animationDuration}
+          infinite={infinite}
+        />
       </div>
     );
   }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,26 +1,46 @@
 import React from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li>
-        <img src="./img/1.png" alt="1" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="2" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="3" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="4" />
-      </li>
-    </ul>
+type Image = {
+  src: string;
+  alt: number;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+type Props = {
+  images: Image[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+  infinite: boolean;
+};
+
+const Carousel: React.FC<Props> = ({
+  images,
+  step,
+  frameSize,
+  itemWidth,
+  animationDuration,
+  infinite,
+}) => {
+  return (
+    <div className="Carousel">
+      <ul className="Carousel__list">
+        <div className="container-short">
+          <div className="container-tall">
+            {images.map(image => (
+              <li key={image.alt}>
+                <img src={image.src} alt={String(image.alt)} />
+              </li>
+            ))}
+          </div>
+        </div>
+      </ul>
+
+      <button type="button">Prev</button>
+      <button type="button">Next</button>
+    </div>
+  );
+};
 
 export default Carousel;
