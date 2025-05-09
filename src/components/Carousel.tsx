bc1538c2572b5d5eabfaac1071f2ac9363b9e3c3
Andrii Filipenko <andreyfil1337@gmail.com>
2025-05-09 16:37:05 +0300

    in progress

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 
 type Image = {
@@ -15,6 +15,11 @@ type Props = {
   infinite: boolean;
 };
 
+enum Direction {
+  right = 1,
+  left = -1,
+}
+
 const Carousel: React.FC<Props> = ({
   images,
   step,
@@ -23,14 +28,40 @@ const Carousel: React.FC<Props> = ({
   animationDuration,
   infinite,
 }) => {
+  const [itemWidthState, setItemWidthState] = useState(itemWidth);
+  const [frameSizeState, setFrameSizeState] = useState(frameSize);
+  const [stepState, setStepState] = useState(step);
+  const [animationDurationState, setAnimationDurationState] =
+    useState(animationDuration);
+
+  function carouselScroll(direction: Direction) {}
+
   return (
     <div className="Carousel">
       <ul className="Carousel__list">
-        <div className="container-short">
-          <div className="container-tall">
+        <div
+          className="container-short"
+          style={{
+            width: `${itemWidthState * frameSizeState}px`,
+          }}
+        >
+          <div
+            className="container-tall"
+            style={{
+              width: `${itemWidthState * images.length}px`,
+              transition: `all ${animationDurationState}ms`,
+              transform: `translate(${itemWidthState * -step}px)`,
+            }}
+          >
             {images.map(image => (
               <li key={image.alt}>
-                <img src={image.src} alt={String(image.alt)} />
+                <img
+                  src={image.src}
+                  alt={String(image.alt)}
+                  style={{
+                    width: `${itemWidthState}px`,
+                  }}
+                />
               </li>
             ))}
           </div>
